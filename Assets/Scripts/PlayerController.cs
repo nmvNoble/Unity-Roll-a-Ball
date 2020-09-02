9b905863c278dd1740b22f0003bea79126cc890a
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-09-02 14:48:14 +0800

    Movement
    Installed Input System Package
    added simple rolling movement for Sphere using Force

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private float _movX, _movY;
+    [SerializeField]
+    private float _speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        _movX = movementVector.x;
+        _movY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(_movX, 0.0f, _movY);
+        _rb.AddForce(movement*_speed);
+    }
+}
